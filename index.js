4c5109561ac13e63152cc4ff955c90760cc6db49
Lautaro Marguery <30993136+ARESLautaro95estudio@users.noreply.github.com>
2024-05-13 21:18:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+class Anuncio
+{
+    constructor(id,titulo,transaccion,descripcion,precio)
+    {
+        this.id = id;
+        this.titulo = titulo;
+        this.transaccion = transaccion;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    mostrar ()
+    {
+    }
+}
